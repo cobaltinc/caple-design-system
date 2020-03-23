77d91eb566a4299cd647c7e4d2f14df48a87508d
kciter <kciter@naver.com>
2020-03-24 00:15:00 +0900

    Remove duplicate code

--- packages/react/stories/02-Collapse.stories.tsx
+++ packages/react/stories/02-Collapse.stories.tsx
@@ -5,55 +5,59 @@ export default {
   title: 'Collapse',
 };
 
+const Content = () => (
+  <Table>
+    <Table.Head>
+      <Table.Cell>ID</Table.Cell>
+      <Table.Cell>Email</Table.Cell>
+      <Table.Cell>Name</Table.Cell>
+      <Table.Cell>Age</Table.Cell>
+    </Table.Head>
+
+    <Table.Body>
+      <Table.Row>
+        <Table.Cell>1</Table.Cell>
+        <Table.Cell>lgross195@ssorgyesdnil.com</Table.Cell>
+        <Table.Cell>Lindsey Gross</Table.Cell>
+        <Table.Cell>24</Table.Cell>
+      </Table.Row>
+
+      <Table.Row>
+        <Table.Cell>1</Table.Cell>
+        <Table.Cell>abuchanan670@nanahcubaerdna.org</Table.Cell>
+        <Table.Cell>Andrea Buchanan</Table.Cell>
+        <Table.Cell>52</Table.Cell>
+      </Table.Row>
+
+      <Table.Row>
+        <Table.Cell>1</Table.Cell>
+        <Table.Cell>dgraham788@mahargaleinad.com</Table.Cell>
+        <Table.Cell>Daniela Graham</Table.Cell>
+        <Table.Cell>37</Table.Cell>
+      </Table.Row>
+
+      <Table.Row>
+        <Table.Cell>1</Table.Cell>
+        <Table.Cell>krichards933@sdrahcirardnassak.net</Table.Cell>
+        <Table.Cell>Kassandra Richards</Table.Cell>
+        <Table.Cell>20</Table.Cell>
+      </Table.Row>
+
+      <Table.Row>
+        <Table.Cell>1</Table.Cell>
+        <Table.Cell>csingh363@hgnisronoc.com</Table.Cell>
+        <Table.Cell>Conor Singh</Table.Cell>
+        <Table.Cell>42</Table.Cell>
+      </Table.Row>
+    </Table.Body>
+  </Table>
+);
+
 export const Default = () => {
   return (
     <div style={{ padding: 30, maxWidth: 600 }}>
       <Collapse title="User List">
-        <Table>
-          <Table.Head>
-            <Table.Cell>ID</Table.Cell>
-            <Table.Cell>Email</Table.Cell>
-            <Table.Cell>Name</Table.Cell>
-            <Table.Cell>Age</Table.Cell>
-          </Table.Head>
-
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>lgross195@ssorgyesdnil.com</Table.Cell>
-              <Table.Cell>Lindsey Gross</Table.Cell>
-              <Table.Cell>24</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>abuchanan670@nanahcubaerdna.org</Table.Cell>
-              <Table.Cell>Andrea Buchanan</Table.Cell>
-              <Table.Cell>52</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>dgraham788@mahargaleinad.com</Table.Cell>
-              <Table.Cell>Daniela Graham</Table.Cell>
-              <Table.Cell>37</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>krichards933@sdrahcirardnassak.net</Table.Cell>
-              <Table.Cell>Kassandra Richards</Table.Cell>
-              <Table.Cell>20</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>csingh363@hgnisronoc.com</Table.Cell>
-              <Table.Cell>Conor Singh</Table.Cell>
-              <Table.Cell>42</Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
+        <Content />
       </Collapse>
     </div>
   );
@@ -63,51 +67,7 @@ export const DefaultExpand = () => {
   return (
     <div style={{ padding: 30, maxWidth: 600 }}>
       <Collapse title="User List" defaultExpand>
-        <Table>
-          <Table.Head>
-            <Table.Cell>ID</Table.Cell>
-            <Table.Cell>Email</Table.Cell>
-            <Table.Cell>Name</Table.Cell>
-            <Table.Cell>Age</Table.Cell>
-          </Table.Head>
-
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>lgross195@ssorgyesdnil.com</Table.Cell>
-              <Table.Cell>Lindsey Gross</Table.Cell>
-              <Table.Cell>24</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>abuchanan670@nanahcubaerdna.org</Table.Cell>
-              <Table.Cell>Andrea Buchanan</Table.Cell>
-              <Table.Cell>52</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>dgraham788@mahargaleinad.com</Table.Cell>
-              <Table.Cell>Daniela Graham</Table.Cell>
-              <Table.Cell>37</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>krichards933@sdrahcirardnassak.net</Table.Cell>
-              <Table.Cell>Kassandra Richards</Table.Cell>
-              <Table.Cell>20</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>csingh363@hgnisronoc.com</Table.Cell>
-              <Table.Cell>Conor Singh</Table.Cell>
-              <Table.Cell>42</Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
+        <Content />
       </Collapse>
     </div>
   );
@@ -117,99 +77,11 @@ export const Animate = () => {
   return (
     <div style={{ padding: 30, maxWidth: 600 }}>
       <Collapse title="Animation" animate>
-        <Table>
-          <Table.Head>
-            <Table.Cell>ID</Table.Cell>
-            <Table.Cell>Email</Table.Cell>
-            <Table.Cell>Name</Table.Cell>
-            <Table.Cell>Age</Table.Cell>
-          </Table.Head>
-
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>lgross195@ssorgyesdnil.com</Table.Cell>
-              <Table.Cell>Lindsey Gross</Table.Cell>
-              <Table.Cell>24</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>abuchanan670@nanahcubaerdna.org</Table.Cell>
-              <Table.Cell>Andrea Buchanan</Table.Cell>
-              <Table.Cell>52</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>dgraham788@mahargaleinad.com</Table.Cell>
-              <Table.Cell>Daniela Graham</Table.Cell>
-              <Table.Cell>37</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>krichards933@sdrahcirardnassak.net</Table.Cell>
-              <Table.Cell>Kassandra Richards</Table.Cell>
-              <Table.Cell>20</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>csingh363@hgnisronoc.com</Table.Cell>
-              <Table.Cell>Conor Singh</Table.Cell>
-              <Table.Cell>42</Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
+        <Content />
       </Collapse>
 
       <Collapse title="Non Animation" animate={false}>
-        <Table>
-          <Table.Head>
-            <Table.Cell>ID</Table.Cell>
-            <Table.Cell>Email</Table.Cell>
-            <Table.Cell>Name</Table.Cell>
-            <Table.Cell>Age</Table.Cell>
-          </Table.Head>
-
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>lgross195@ssorgyesdnil.com</Table.Cell>
-              <Table.Cell>Lindsey Gross</Table.Cell>
-              <Table.Cell>24</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>abuchanan670@nanahcubaerdna.org</Table.Cell>
-              <Table.Cell>Andrea Buchanan</Table.Cell>
-              <Table.Cell>52</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>dgraham788@mahargaleinad.com</Table.Cell>
-              <Table.Cell>Daniela Graham</Table.Cell>
-              <Table.Cell>37</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>krichards933@sdrahcirardnassak.net</Table.Cell>
-              <Table.Cell>Kassandra Richards</Table.Cell>
-              <Table.Cell>20</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>csingh363@hgnisronoc.com</Table.Cell>
-              <Table.Cell>Conor Singh</Table.Cell>
-              <Table.Cell>42</Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
+        <Content />
       </Collapse>
     </div>
   );
@@ -219,105 +91,17 @@ export const CaretText = () => {
   return (
     <div style={{ padding: 30, maxWidth: 600 }}>
       <Collapse title="User List" caretText={{ openText: 'Open', closeText: 'Close' }}>
-        <Table>
-          <Table.Head>
-            <Table.Cell>ID</Table.Cell>
-            <Table.Cell>Email</Table.Cell>
-            <Table.Cell>Name</Table.Cell>
-            <Table.Cell>Age</Table.Cell>
-          </Table.Head>
-
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>lgross195@ssorgyesdnil.com</Table.Cell>
-              <Table.Cell>Lindsey Gross</Table.Cell>
-              <Table.Cell>24</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>abuchanan670@nanahcubaerdna.org</Table.Cell>
-              <Table.Cell>Andrea Buchanan</Table.Cell>
-              <Table.Cell>52</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>dgraham788@mahargaleinad.com</Table.Cell>
-              <Table.Cell>Daniela Graham</Table.Cell>
-              <Table.Cell>37</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>krichards933@sdrahcirardnassak.net</Table.Cell>
-              <Table.Cell>Kassandra Richards</Table.Cell>
-              <Table.Cell>20</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>csingh363@hgnisronoc.com</Table.Cell>
-              <Table.Cell>Conor Singh</Table.Cell>
-              <Table.Cell>42</Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
+        <Content />
       </Collapse>
     </div>
   );
 };
 
-export const Color = () => {
+export const Styling = () => {
   return (
     <div style={{ padding: 30, maxWidth: 600 }}>
       <Collapse title="User List" caretText={{ openText: 'Open', closeText: 'Close' }} caretTextColor="green" titleColor="red" caretColor="blue">
-        <Table>
-          <Table.Head>
-            <Table.Cell>ID</Table.Cell>
-            <Table.Cell>Email</Table.Cell>
-            <Table.Cell>Name</Table.Cell>
-            <Table.Cell>Age</Table.Cell>
-          </Table.Head>
-
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>lgross195@ssorgyesdnil.com</Table.Cell>
-              <Table.Cell>Lindsey Gross</Table.Cell>
-              <Table.Cell>24</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>abuchanan670@nanahcubaerdna.org</Table.Cell>
-              <Table.Cell>Andrea Buchanan</Table.Cell>
-              <Table.Cell>52</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>dgraham788@mahargaleinad.com</Table.Cell>
-              <Table.Cell>Daniela Graham</Table.Cell>
-              <Table.Cell>37</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>krichards933@sdrahcirardnassak.net</Table.Cell>
-              <Table.Cell>Kassandra Richards</Table.Cell>
-              <Table.Cell>20</Table.Cell>
-            </Table.Row>
-
-            <Table.Row>
-              <Table.Cell>1</Table.Cell>
-              <Table.Cell>csingh363@hgnisronoc.com</Table.Cell>
-              <Table.Cell>Conor Singh</Table.Cell>
-              <Table.Cell>42</Table.Cell>
-            </Table.Row>
-          </Table.Body>
-        </Table>
+        <Content />
       </Collapse>
     </div>
   );
