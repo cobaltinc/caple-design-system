86c5e7c76474199c4530f8da628a4d80ff028036
kciter <kciter@naver.com>
2020-03-23 23:40:41 +0900

    Add Spacer size type

--- packages/react/src/components/Spacer/Spacer.tsx
+++ packages/react/src/components/Spacer/Spacer.tsx
@@ -3,7 +3,7 @@ import classnames from 'classnames';
 import ConfigContext from '../_config/ConfigContext';
 import './Spacer.style.scss';
 
-export type SpacerSizeType = 'small' | 'normal' | 'large';
+export type SpacerSizeType = 'tiny' | 'normal' | 'large';
 export type SpacerOrientationType = 'horizontal' | 'vertical';
 
 export interface SpacerProps {
@@ -18,15 +18,13 @@ export default ({ type = 'horizontal', size = 'normal', className = '', style }:
   const { prefix } = useContext(ConfigContext);
   const classPrefix = `${prefix}-spacer`;
   const classNames = classnames(classPrefix, className, `${classPrefix}--type-${type}`, {
-    [`${classPrefix}--size-${size}`]: typeof size === 'string'
+    [`${classPrefix}--size-${size}`]: typeof size === 'string',
   });
 
   const sizeStyle = {
     width: type === 'vertical' && typeof size === 'number' ? size : undefined,
-    height: type === 'horizontal' && typeof size === 'number' ? size : undefined
+    height: type === 'horizontal' && typeof size === 'number' ? size : undefined,
   };
 
-  return (
-    <div className={classNames} style={{ ...style, ...sizeStyle }} />
-  );
+  return <div className={classNames} style={{ ...style, ...sizeStyle }} />;
 };
