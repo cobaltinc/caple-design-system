ec56dd3d61e15a5fee869fc956363b7243b8b9e3
kciter <kciter@naver.com>
2020-04-17 19:55:46 +0900

    Fix Popover story

--- packages/react/stories/02-Popover.stories.tsx
+++ packages/react/stories/02-Popover.stories.tsx
@@ -6,7 +6,7 @@ export default {
 };
 
 const CommonPopoverContent = () => (
-  <div>
+  <div style={{ padding: '12px 20px' }}>
     <Text paragraph>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
