d460d42d5c74ca47045f80f3b1148d5bba59034d
kciter <kciter@naver.com>
2020-08-02 20:59:00 +0900

    Update Switch component

--- packages/react/src/components/Switch/SwitchGroup.tsx
+++ packages/react/src/components/Switch/SwitchGroup.tsx
@@ -2,43 +2,66 @@ import React, { useReducer } from 'react';
 import classnames from 'classnames';
 import ConfigContext from '../_config/ConfigContext';
 import { SwitchProps } from './Switch';
-import { warning, convertReactNodeTo } from '../../utils';
+import { convertReactNodeTo } from '../../utils';
 import './Switch.style.scss';
-import SwitchContext, { SwitchReducer } from './SwitchContext';
+import SwitchContext, { SwitchItemType, SwitchReducer } from './SwitchContext';
 
 interface SwitchGroupProps {
   children: React.ReactNode;
   multiple?: boolean;
   defaultActive?: string | string[];
-  onChange?(values: string[]): void;
+  onChange?(values?: SwitchItemType | SwitchItemType[]): void;
   className?: string;
   style?: React.CSSProperties;
 }
 
 export default ({ children, multiple = false, defaultActive, onChange, className = '', style, ...props }: SwitchGroupProps) => {
-  const { useContext, useState, useEffect } = React;
+  const { useContext, useEffect } = React;
   const { prefix } = useContext(ConfigContext);
   const classPrefix = `${prefix}-switch-group`;
   const classNames = classnames(classPrefix, className);
 
   const [state, dispatch] = useReducer(SwitchReducer, {
     multiple,
-    active: defaultActive
-      ? Array.isArray(defaultActive)
-        ? []
-        : {
-            id: defaultActive,
-            title: '',
-            value: '',
-          }
-      : undefined,
+    active: (() => {
+      if (defaultActive) {
+        if (Array.isArray(defaultActive)) {
+          return React.Children.toArray(children)
+            .map((switchComponent: any) => {
+              const switchProps = (switchComponent as React.ReactElement<SwitchProps>).props;
+              return {
+                id: switchProps.id ?? '',
+                value: switchProps.value,
+                title: switchProps.title,
+              };
+            })
+            .filter(switchItem => switchItem.id && defaultActive.includes(switchItem.id)) as SwitchItemType[];
+        } else {
+          const switchItems = React.Children.toArray(children).map((switchComponent: any) => {
+            const switchProps = (switchComponent as React.ReactElement<SwitchProps>).props;
+            return {
+              id: switchProps.id ?? '',
+              value: switchProps.value,
+              title: switchProps.title,
+            };
+          }) as SwitchItemType[];
+
+          return switchItems.find(switchItem => switchItem.id && switchItem.id === defaultActive);
+        }
+      } else if (multiple) {
+        return [];
+      }
+    })(),
   });
 
-  const switchs = convertReactNodeTo<SwitchProps>('Switch.Group', 'Switch', children).map((element: any, index, elements) => {
-    return React.cloneElement<SwitchProps>(element, {
-      checked: !multiple ? element.props.id === (state.active as any)?.id : element.props.checked,
+  const switchs = convertReactNodeTo<SwitchProps>('Switch.Group', 'Switch', children).map((element, index, elements) => {
+    const switchComponent = element as React.ReactElement<SwitchProps>;
+    return React.cloneElement<SwitchProps>(switchComponent, {
+      checked: !multiple
+        ? switchComponent.props.id === (state?.active as any)?.id
+        : (state?.active as any).map((item: any) => item.id).includes(switchComponent.props.id),
       style: {
-        ...element.props.style,
+        ...switchComponent.props.style,
         borderTopRightRadius: index !== elements.length - 1 ? 0 : undefined,
         borderBottomRightRadius: index !== elements.length - 1 ? 0 : undefined,
         borderTopLeftRadius: index !== 0 ? 0 : undefined,
@@ -47,6 +70,11 @@ export default ({ children, multiple = false, defaultActive, onChange, className
       },
     });
   });
+
+  useEffect(() => {
+    console.log(state.active);
+    onChange?.(state.active);
+  }, [state.active]);
 
   return (
     <SwitchContext.Provider value={{ state, dispatch }}>
